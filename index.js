779ebcee9d77dbd7753c58db15af066ba2db042e
Ashwin746544 <ahpatel99999@gmail.com>
2022-02-01 18:46:08 -0800

    feat: make some section responsive design

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var hambergerIcon = document.querySelector(".hamberger");
 var navbar = document.querySelector("nav");
+var mediaQuary = window.matchMedia("(max-width: 575px)");
+var changedToh2 = document.querySelector(".changedToh2");
 
 hambergerIcon.addEventListener("click", () => {
   navbar.classList.toggle("nav-active");
 })
+
+mediaQuary.addListener(change);
+function change() {
+  if (mediaQuary.matches) {
+    changedToh2.outerHTML = "<h2 class='myH2'>" + changedToh2.innerHTML + "</h2>";
+    console.log("fhhfjhf");
+  }
+}
